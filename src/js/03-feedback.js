a7ba7399a5b2b8b9ad29d237b2689468bad9cafe
MaxF1996 <maxeffect.apollo@gmail.com>
2022-12-16 20:10:57 +0200

    feedback currently done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const formData = {};
+
+const onFormData = event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onSubmitForm = event => {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('[name="email"]');
+  const message = document.querySelector('[name="message"]');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
